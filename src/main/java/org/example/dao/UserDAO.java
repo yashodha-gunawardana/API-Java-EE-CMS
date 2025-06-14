2145df93d71a17f80109ba8ddf375a7b207aebf6
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-06-14 17:41:52 +0530

    add findUser method to fetch user using UserDAO and ServletContext

--- src/main/java/org/example/dao/UserDAO.java
+++ src/main/java/org/example/dao/UserDAO.java
@@ -1,4 +1,9 @@
 package org.example.dao;
 
+import jakarta.servlet.ServletContext;
+import org.example.dto.UserDTO;
+
 public class UserDAO {
+    public static UserDTO findUser(ServletContext servletContext, String email, String password, String role) {
+    }
 }
--- src/main/java/org/example/model/UserModel.java
+++ src/main/java/org/example/model/UserModel.java
@@ -1,9 +1,11 @@
 package org.example.model;
 
 import jakarta.servlet.ServletContext;
+import org.example.dao.UserDAO;
 import org.example.dto.UserDTO;
 
 public class UserModel {
     public static UserDTO findUser(ServletContext servletContext, String email, String password, String role) {
+        return UserDAO.findUser(servletContext, email, password, role);
     }
 }
