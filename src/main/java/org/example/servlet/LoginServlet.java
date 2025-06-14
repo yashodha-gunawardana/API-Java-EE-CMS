65a461e99eff8be6bb7f5012d60e8a47795f8ba3
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-06-14 16:48:24 +0530

    create loginServlet class with doGet method

--- src/main/java/org/example/servlet/LoginServlet.java
+++ src/main/java/org/example/servlet/LoginServlet.java
@@ -1,7 +1,16 @@
 package org.example.servlet;
 
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-public class LoginServlet {
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
 
+    @Override
+    protected void doGet (HttpServletRequest req, HttpServletResponse resp) {
+
+    }
 
 }
