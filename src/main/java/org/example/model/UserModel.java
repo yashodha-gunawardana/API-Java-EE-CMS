b7f670c4261308d9efc0ca9503e82c3084812063
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-06-14 16:56:04 +0530

    use ServletContext to call UserModel.findUser for user authentication

--- src/main/java/org/example/model/UserModel.java
+++ src/main/java/org/example/model/UserModel.java
@@ -1,4 +1,9 @@
 package org.example.model;
 
+import jakarta.servlet.ServletContext;
+import org.example.dto.UserDTO;
+
 public class UserModel {
+    public static UserDTO findUser(ServletContext servletContext, String email, String password, String role) {
+    }
 }
--- src/main/java/org/example/servlet/LoginServlet.java
+++ src/main/java/org/example/servlet/LoginServlet.java
@@ -1,9 +1,12 @@
 package org.example.servlet;
 
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.dto.UserDTO;
+import org.example.model.UserModel;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
@@ -13,6 +16,9 @@ public class LoginServlet extends HttpServlet {
         String email = req.getParameter("email");
         String password = req.getParameter("password");
         String role = req.getParameter("userRole");
+
+        ServletContext servletContext = getServletContext();
+        UserDTO user = UserModel.findUser(servletContext, email, password, role);
     }
 
 }
