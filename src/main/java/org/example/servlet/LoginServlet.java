45926f99ea5112fbd625e8c84424a494f782538b
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-06-14 17:09:36 +0530

    call UserModel to find user and redirect on invalid credentials

--- src/main/java/org/example/servlet/LoginServlet.java
+++ src/main/java/org/example/servlet/LoginServlet.java
@@ -8,17 +8,23 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.example.dto.UserDTO;
 import org.example.model.UserModel;
 
+import java.io.IOException;
+
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
     @Override
-    protected void doGet (HttpServletRequest req, HttpServletResponse resp) {
+    protected void doPost (HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String email = req.getParameter("email");
         String password = req.getParameter("password");
         String role = req.getParameter("userRole");
 
         ServletContext servletContext = getServletContext();
         UserDTO user = UserModel.findUser(servletContext, email, password, role);
+
+        if (user == null) {
+            resp.sendRedirect("Login.jsp?error= Invalid email or password");
+        }
     }
 
 }
