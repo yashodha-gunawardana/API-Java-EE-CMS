00e1b70d485a3b6e963f27baf13d6040f353097c
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-06-14 18:08:38 +0530

    get the connection pool (BasicDataSource) from ServletContext

--- src/main/java/org/example/dao/UserDAO.java
+++ src/main/java/org/example/dao/UserDAO.java
@@ -1,9 +1,14 @@
 package org.example.dao;
 
 import jakarta.servlet.ServletContext;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.example.dto.UserDTO;
 
+import java.sql.SQLException;
+
 public class UserDAO {
-    public static UserDTO findUser(ServletContext servletContext, String email, String password, String role) {
-    }
+    public static UserDTO findUser(ServletContext servletContext, String email, String password, String role) throws SQLException {
+
+        BasicDataSource dataSource = (BasicDataSource) servletContext.getAttribute("ds");
+
 }
